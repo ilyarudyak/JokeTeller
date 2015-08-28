fe560773d8ee8befa6e5aafb1fc7d6763c212fff
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-08-28 14:00:39 +0300

    Add Joke class

---
+++ jokeapi/src/main/java/com/ilyarudyak/joketeller/jokeapi/Joke.java
@@ -0,0 +1,17 @@
+package com.ilyarudyak.joketeller.jokeapi;
+
+/**
+ * Created by ilyarudyak on 8/28/15.
+ */
+public class Joke {
+
+    private String joke;
+
+    public String getJoke() {
+        return joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+}
